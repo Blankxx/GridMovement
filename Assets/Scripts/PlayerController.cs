e8be0d6d2fcad719e29dd936cdd342b78bf535e2
Blankxx <xxblankxx000@gmail.com>
2017-03-11 16:03:28 -0600

    commit
    commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -27,25 +27,22 @@ public class PlayerController : MonoBehaviour
 	}
 	void Update ()
 	{
-		Move ();
-	}
-	void FixedUpdate ()
-	{
 		Raycasting ();
+		Move ();
 	}
 	void Move ()
 	{
-		if (Input.GetAxisRaw ("Horizontal") > 0 && transform.position == pos && !rightBlocked || Input.GetAxisRaw ("Horizontal") < 0 && transform.position == pos && !leftBlocked)
-		{
-			isMoving = true;
-			pos += new Vector3 (Input.GetAxisRaw ("Horizontal"), 0f, 0f);
-			lastMove = new Vector2 (Input.GetAxisRaw ("Horizontal"), 0f);
-		}
 		if (Input.GetAxisRaw ("Vertical") > 0 && transform.position == pos && !upBlocked || Input.GetAxisRaw ("Vertical") < 0 && transform.position == pos && !downBlocked)
 		{
 			isMoving = true;
 			pos += new Vector3 (0f, Input.GetAxisRaw ("Vertical"), 0f);
 			lastMove = new Vector2 (0f, Input.GetAxisRaw ("Vertical"));
+		}
+		if (Input.GetAxisRaw ("Horizontal") < 0 && transform.position == pos && !leftBlocked || Input.GetAxisRaw ("Horizontal") > 0 && transform.position == pos && !rightBlocked)
+		{
+			isMoving = true;
+			pos += new Vector3 (Input.GetAxisRaw ("Horizontal"), 0f, 0f);
+			lastMove = new Vector2 (Input.GetAxisRaw ("Horizontal"), 0f);
 		}
 		transform.position = Vector3.MoveTowards (transform.position, pos, Time.deltaTime * speed);
 		if (transform.position == Vector3.MoveTowards (transform.position, pos, Time.deltaTime * speed))
@@ -60,14 +57,14 @@ public class PlayerController : MonoBehaviour
 	}
 	void Raycasting ()
 	{
-		Debug.DrawLine (rayUpStart.position, rayUpEnd.position, Color.red);
-		Debug.DrawLine (rayDownStart.position, rayDownEnd.position, Color.red);
-		Debug.DrawLine (rayLeftStart.position, rayLeftEnd.position, Color.red);
-		Debug.DrawLine (rayRightStart.position, rayRightEnd.position, Color.red);
-
+		StartCoroutine (Delay());
 		upBlocked = Physics2D.Linecast (rayUpStart.position, rayUpEnd.position, 1 << LayerMask.NameToLayer ("Obstacle"));
 		downBlocked = Physics2D.Linecast (rayDownStart.position, rayDownEnd.position, 1 << LayerMask.NameToLayer ("Obstacle"));
 		leftBlocked = Physics2D.Linecast (rayLeftStart.position, rayLeftEnd.position, 1 << LayerMask.NameToLayer ("Obstacle"));
 		rightBlocked = Physics2D.Linecast (rayRightStart.position, rayRightEnd.position, 1 << LayerMask.NameToLayer ("Obstacle"));
+	}
+	IEnumerator Delay()
+	{
+		yield return new WaitForSeconds (1);
 	}
 }
